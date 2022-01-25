44bc56ff8328193057acda9155d18814f9df73e4
Santiago <santi99oca@gmail.com>
2022-01-25 02:31:13 -0300

    Implementacion de csv para guardar un output ordenado

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
+import csv
+
+
 def normalize(text):
 # Signos de puntuacion comunes y caracteres que no forman una palabra
     punctuation_marks = list(".,:;?¿()'><")
-# reemplaza los signos de puntuacion por espacios, solo por si no hay buena gramatica en el texto
 
     words = [word.lower() for word in text.split()]
-
+# reemplaza los signos de puntuacion por espacios, solo por si no hay buena gramatica en el texto
     for index, word in enumerate(words):
         for mark in punctuation_marks:
             word = word.replace(mark, "")
@@ -23,11 +25,16 @@ def run():
                 count += 1
                 total_count += 1
                 res[word] = count
-    
-    print(res)
+
+    words_list = sorted(res)
+#Abre el archivo del output y lo sobrescribe para guardar las palabras en un csv
+    output_file = open("output.csv", "w")
+    writer = csv.writer(output_file)
+    writer.writerow(['word', 'count'])
+#Escribe cada palabra del diccionario con su valor
+    for word in words_list:
+        writer.writerow([word, res[word]])
         
     
-
-
 if __name__ == '__main__':
     run()
