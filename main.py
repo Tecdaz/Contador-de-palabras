b545e661997ebcd5244287acf2dc83a1aec82330
Santiago <santi99oca@gmail.com>
2022-01-25 01:51:33 -0300

    Primer version del programa, Separa las palabras pero no le quita los signos de puntuacion

---
+++ main.py
@@ -0,0 +1,33 @@
+def normalize(text):
+# Signos de puntuacion comunes y caracteres que no forman una palabra
+    punctuation_marks = list(".,:;?¿()'><")
+# reemplaza los signos de puntuacion por espacios, solo por si no hay buena gramatica en el texto
+
+    words = [word.lower() for word in text.split()]
+
+    for word in words:
+        for mark in punctuation_marks:
+            word.replace(mark, "")
+
+    return words
+
+def run():
+    total_count = 0
+    res = {}
+
+    with open("./data.txt", "r") as text:
+
+        for line in text:
+            for word in normalize(line):
+                count = res.get(word, 0)
+                count += 1
+                total_count += 1
+                res[word] = count
+    
+    print(res)
+        
+    
+
+
+if __name__ == '__main__':
+    run()
